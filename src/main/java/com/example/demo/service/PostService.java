fd1c08c3f8fb0f8b7c298c72eddca8fc31c9aecd
Yana Gnedkova <ygnedkova@gmail.com>
2024-02-06 17:30:23 +0500

    модель

---
+++ src/main/java/com/example/demo/service/PostService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Post;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class PostService {
+    public List<Post> listAllPosts()
+    {
+        return Arrays.asList(new Post("post 1"), new Post("post 2"), new Post("post 3"));
+    }
+
+}
